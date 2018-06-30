8bf2278e433c3aa1db7373f7c6af586c4cd2343f
princyprincy <38650887+princyprincy@users.noreply.github.com>
2018-06-30 07:10:06 +0000

    Update vowel.c

--- beginner/vowel.c
+++ beginner/vowel.c
@@ -3,9 +3,16 @@ int main()
 {
 	char n;
 	scanf("%c",&n);
+	if((n>='a'&& n<='z')||(n>='A'&& n<='Z'))
+	{
 	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
 		printf("Vowel");
 	else
 		printf("Consonant");
+	}
+	else
+	{
+		printf("invalid");
+	}
 	return 0;
 }
