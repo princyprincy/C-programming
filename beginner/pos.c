2bf35b7b9ea425861d75aec4aeeaca81354b1240
princyprincy <38650887+princyprincy@users.noreply.github.com>
2018-06-30 06:27:13 +0000

    Update pos.c

--- beginner/pos.c
+++ beginner/pos.c
@@ -2,12 +2,15 @@
 int main()
 {
 	int a;
-	scanf("%d",&a);
-	if(a>0)
-		printf("Positive");
-	else if(a<0)
-		printf("Negative");
-	else
-		printf("Zero");
+	if(1<=a<=10000)
+	{
+		scanf("%d",&a);
+		if(a>0)
+			printf("Positive");
+		else if(a<0)
+			printf("Negative");
+		else
+			printf("Zero");
+	}
 	return 0;
 }
