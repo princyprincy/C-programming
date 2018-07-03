0ac379bb52fbf4fa05e835846c82d5761d0e7456
princyprincy <38650887+princyprincy@users.noreply.github.com>
2018-07-03 10:43:36 +0000

    Add files via upload

---
+++ beginner/odd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int N,Q,i;
+	scanf("%d",&N);
+	scanf("%d",&Q);
+	if(Q<=100000)
+	{
+	for(i=N;i<=Q;i++)
+	{
+		while(i%2!=0)
+		{
+			printf("%d\n",i);
+			break;
+		}
+	}
+	}
+	else
+	{
+		printf("invalid");
+	}
+	return 0;
+}
