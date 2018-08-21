e8fd6405f352900e535451892824a00a4b9aecd6
princyprincy <38650887+princyprincy@users.noreply.github.com>
2018-08-21 10:16:52 +0000

    Add files via upload

---
+++ beginner/sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+	int array[30],i,j,t,n;
+	scanf("%d", &n);
+	for(i=0;i<n;++i)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<n;++i) 
+	{
+		for(j=i+1;j<n;++j)
+		{
+			if(array[i]>array[j]) 
+			{
+				t=array[i];
+				array[i]=array[j];
+				array[j]=t;
+			}
+		}
+	}
+	for(i=0;i<n;++i)
+	{
+		printf("%d\n",array[i]);
+	}
+}
