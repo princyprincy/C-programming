9930a130509bac67123ba103adfc6ba49f5ff06c
princyprincy <38650887+princyprincy@users.noreply.github.com>
2018-08-21 10:39:47 +0000

    Add files via upload

---
+++ beginner/arme.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+	int array[10],i,j,t,n,m;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<n;i++) 
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(array[i]>array[j]) 
+			{
+				t=array[i];
+				array[i]=array[j];
+				array[j]=t;
+			}
+		}
+	}
+	if(n%2==0)
+	{
+		m=(array[n-1/2]+(array[n/2]))/2;
+	}
+	else
+	{
+		m=array[n/2];
+	}
+	printf("\n%d",m);
+}
