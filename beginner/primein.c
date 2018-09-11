8be362b95e61d8758b7e6ef253b48e841f35c047
princyprincy <38650887+princyprincy@users.noreply.github.com>
2018-09-11 10:44:48 +0000

    Add files via upload

---
+++ beginner/primein.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int low,high,i,j,flag;
+	scanf("%d%d",&low,&high);
+	for(i=low+1;i<high;i++)
+	{
+		flag = 0;
+		for(j=2;j<=i/2;++j)
+		{
+			if(i%j==0)
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+			printf("%d ",i);
+	}
+	return 0;
+}
